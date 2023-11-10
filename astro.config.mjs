a7b843fe675e64ea9106217a4b79969c58e15da5
Brenno Rodrigues <brennopereira6@gmail.com>
2023-11-10 01:00:18 -0300

    chore: adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import critters from "astro-critters";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), critters()]
+  integrations: [tailwind(), critters(), sitemap()]
 });
