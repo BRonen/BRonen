07454f2621a423f8ec924d9381c3599f96d7b8b6
Brenno Rodrigues <brennopereira6@gmail.com>
2023-11-10 00:57:41 -0300

    chore: adding astro-critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), critters()]
 });
