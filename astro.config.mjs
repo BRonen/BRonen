582a7a8901c6940d2a37e893ba0bdbc2f8e048b9
Brenno Rodrigues <brennopereira6@gmail.com>
2023-11-10 01:01:05 -0300

    chore: adding prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import critters from "astro-critters";
-
 import sitemap from "@astrojs/sitemap";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), critters(), sitemap()]
+  integrations: [tailwind(), critters(), sitemap(), prefetch()]
 });
