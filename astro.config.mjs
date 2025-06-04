d65d324568ba4b2e03b338dfa16d0ab8c2d7168f
BRonen <brennopereira6@gmail.com>
2025-06-03 21:18:55 -0300

    chore: add cf workers setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: "https://bronen.com.br",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [sitemap()],
+  adapter: cloudflare(),
 });
