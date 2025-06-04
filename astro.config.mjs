24afcdc92db90f4bb8525f6b6bf991bc31cf2be6
BRonen <brennopereira6@gmail.com>
2025-06-03 21:11:15 -0300

    chore: upgrade tailwind to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import critters from "astro-critters";
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: "https://bronen.com.br",
-  integrations: [tailwind(), critters(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [sitemap()],
 });
